ba73235b7d09c37796d344ba5128105988cc090b
Madekuji <marcelo.matthew100@gmail.com>
2021-01-07 17:31:12 +0800

    Added Japanese text support + more witty lines

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var titles = [
     "\"I think it's about time we have a TED talk on VTubers.\"",
     "\"Watch that CPS counter, yo\"",
     ":AyameDespair:",
-    "\"These are the alternatives you are looking for.\""
+    "\"These are the alternatives you are looking for.\"",
+    "\"Keep an eye out for when a translator does a translation stream!\"",
+    "「どこ見てんだよ？」",
+    "\"Yes, good subs do exist.\"",
+    "\"It doesn't hurt to have higher standards.\"",
+    "\"This is your reminder that your favorite VTuber might be streaming right now, and YouTube probably forgot to notify you. Again.\"",
+    "「スーパーチャットターイム」"
  ];
 
  function newTitle () {
